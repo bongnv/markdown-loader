97701e8d14be2146d1fb27f12fdd6a885f9b1690
Bong Nguyen <van.bong@grabtaxi.com>
2020-05-25 11:43:14 +0800

    chore: add dependencies

--- packages/next-markdown-plugin/index.js
+++ packages/next-markdown-plugin/index.js
@@ -13,6 +13,28 @@ module.exports = (nextConfig = {}) => {
         }
       });
 
+      // TODO: assert when module is not found
+      config.module.rules.push({
+        test: /\.(jpg|jpeg|png|svg|webp|gif|ico)$/,
+        issuer: /\.md$/,
+        use: [
+          "image-trace-loader",
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: `${options.isServer ? '../' : ''}static/images/`,
+              publicPath: "/_next/static/images",
+            }
+          },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              disable: options.dev,
+            }
+          },
+        ]
+      })
+
       if (typeof nextConfig.webpack === "function") {
         return nextConfig.webpack(config, options);
       }
